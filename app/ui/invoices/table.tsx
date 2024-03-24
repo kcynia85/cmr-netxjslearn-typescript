d0e84c3f083ff3be11e0237b9dfc484524e5abb7
Kamil Lazarski <lazarski.k.s@gmail.com>
2024-03-24 19:35:43 +0100

    Optimized mobile invoice display by destructuring props and integrated debounced search function to enhance UI responsiveness and search performance.

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -18,37 +18,45 @@ export default async function InvoicesTable({
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            {invoices?.map((invoice) => (
+            {invoices?.map(({ 
+              id, 
+              image_url, 
+              name, 
+              email, 
+              status, 
+              amount, 
+              date 
+            }) => (
               <div
-                key={invoice.id}
+                key={id}
                 className="mb-2 w-full rounded-md bg-white p-4"
               >
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
-                        src={invoice.image_url}
+                        src={image_url}
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt={`${invoice.name}'s profile picture`}
+                        alt={`${name}'s profile picture`}
                       />
-                      <p>{invoice.name}</p>
+                      <p>{name}</p>
                     </div>
-                    <p className="text-sm text-gray-500">{invoice.email}</p>
+                    <p className="text-sm text-gray-500">{email}</p>
                   </div>
-                  <InvoiceStatus status={invoice.status} />
+                  <InvoiceStatus status={status} />
                 </div>
                 <div className="flex w-full items-center justify-between pt-4">
                   <div>
                     <p className="text-xl font-medium">
-                      {formatCurrency(invoice.amount)}
+                      {formatCurrency(amount)}
                     </p>
-                    <p>{formatDateToLocal(invoice.date)}</p>
+                    <p>{formatDateToLocal(date)}</p>
                   </div>
                   <div className="flex justify-end gap-2">
-                    <UpdateInvoice id={invoice.id} />
-                    <DeleteInvoice id={invoice.id} />
+                    <UpdateInvoice id={id} />
+                    <DeleteInvoice id={id} />
                   </div>
                 </div>
               </div>
