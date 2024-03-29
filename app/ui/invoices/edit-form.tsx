fc997a96888713b3b924cab1159f7b9ad1f6943e
Kamil Lazarski <lazarski.k.s@gmail.com>
2024-03-29 22:42:25 +0100

    Enhanced invoice editing functionality with edit page setup, update actions, and form adjustments
    Enhanced the invoice editing experience with new edit page, update
    functionality, and UI tweaks

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,9 +18,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        <h3 className="mb-2 block text-sm font-medium"> Invoice No.</h3>
+        <div className="pb-2 text-gray-500">{invoice.id}</div>
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -31,6 +36,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              disabled
             >
               <option value="" disabled>
                 Select a customer
@@ -109,7 +115,7 @@ export default function EditInvoiceForm({
           </div>
         </fieldset>
       </div>
-      <div className="mt-6 flex justify-end gap-4">
+      <div className="mt-6 flex justify-start gap-4">
         <Link
           href="/dashboard/invoices"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
