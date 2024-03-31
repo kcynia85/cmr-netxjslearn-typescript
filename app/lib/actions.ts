43a97cd098d8f388ecbf31ce55ed663541952cf8
Kamil Lazarski <lazarski.k.s@gmail.com>
2024-03-31 18:18:14 +0200

    Added authentication with NextAuth, including login page and sign out functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -31,20 +33,20 @@ export type State = {
 };
 
 export async function createInvoice(prevState: State, formData: FormData) {
-  const validatedFields = CreateInvoice.safeParse({
+  const validatedFields = CreateInvoice.safeParse({ 
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  });
+  }); //
  
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: 'Missing Fields. Failed to Create Invoice.',
     };
-  }
+  } //
 
-  const { customerId, amount, status } = validatedFields.data;
+  const { customerId, amount, status } = validatedFields.data; //
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
  
@@ -103,5 +105,24 @@ export async function deleteInvoice(id: string) {
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
